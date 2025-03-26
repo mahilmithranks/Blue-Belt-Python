7acbc20a40c605a72a146ef3b616cffd22cad19d
mahilmithranks <mahilmithranks2007@gmail.com>
2025-03-26 13:28:08 +0530

    done

---
+++ RotateMatrixby90.py
@@ -0,0 +1,16 @@
+n = int(input())
+store = []
+for i in range(n):
+    row = list(map(int,input().split()))
+    store.append(row)
+
+for i in range(n):
+    for j in range(i+1,n):
+        store[i][j],store[j][i] = store[j][i],store[i][j]
+
+for i in range(n):
+    for j in range(n//2):
+        store[i][j],store[i][n-1-j]=store[i][n-1-j],store[i][j]
+for i in store:
+    print(*i)
+print("__")
