7a9ac7c950fc03a80d2e1fcc0264486594e0c59b
mahilmithranks <mahilmithranks2007@gmail.com>
2025-04-01 16:50:13 +0530

    fresh

--- SubarrayPrinting.py
+++
@@ -1,8 +0,0 @@
-arr = list(map(int,input().split()))
-subarray=[]
-for i in range(len(arr)):
-    for j in range(i,len(arr)):
-        store = arr[i:j+1]
-        subarray.append(store)
-for i in subarray:
-    print(*i)
